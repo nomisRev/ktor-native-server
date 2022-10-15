dd96e63dc846fd6431e69778f7d0ee0f8ed4c50e
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-10-15 21:32:43 +0200

    Setup a postgres test

--- src/commonTest/kotlin/com/fortysevendegrees/PingSpec.kt
+++ src/commonTest/kotlin/com/fortysevendegrees/PingSpec.kt
@@ -1,5 +1,6 @@
 package com.fortysevendegrees
 
+import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.ktor.client.request.get
@@ -16,6 +17,34 @@ class PingSpec : StringSpec({
       val response = client.get("/ping")
       response.status shouldBe HttpStatusCode.OK
       response.bodyAsText() shouldBe "pong"
+    }
+  }
+  
+  val email = "my-email@gmail.com"
+  val username = "my-username"
+  val pw = "non-encrypted"
+  val bio = "my-bio"
+  val image = "www.gravitar.com/my-username"
+  
+  "Postgres test" {
+    postgres(Env.Postgres()).use { sqlDelight ->
+      val userId = sqlDelight.usersQueries.insertAndGetId(
+        "my-email@gmail.com",
+        "my-username",
+        "non-encrypted".encodeToByteArray(),
+        "my-bio",
+        "www.gravitar.com/my-username"
+      ).executeAsOne()
+      
+      val selecyById = sqlDelight.usersQueries.selectById(userId).executeAsOne()
+      
+      assertSoftly {
+        selecyById.email shouldBe email
+        selecyById.username shouldBe username
+        selecyById.hashed_password.decodeToString() shouldBe pw
+        selecyById.bio shouldBe bio
+        selecyById.image shouldBe image
+      }
     }
   }
 })
