5d29fc755b173ac39c1cc740f27ded8fdcbdcb4b
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-10-16 18:37:22 +0200

    Build matrix

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,10 +32,23 @@ fun KotlinNativeTargetWithHostTests.setup() =
     executable { entryPoint = "com.fortysevendegrees.main" }
   }
 
+println(System.getProperty("os.name"))
+
 kotlin {
-  // linuxX64 { setup() }
-  macosX64 { setup() }
-  // macosArm64 { setup() }
+  if (System.getenv("CI").toBoolean()) {
+    when(System.getProperty("os.name")) {
+      "Mac OS X" -> {
+        macosX64 { setup() }
+        macosArm64 { setup() }
+      }
+      "Linux" -> linuxX64 { setup() }
+      null -> throw GradleException("null os.name.")
+    }
+  } else {
+    linuxX64 { setup() }
+    macosX64 { setup() }
+    macosArm64 { setup() }
+  }
   
   sourceSets {
     val commonMain by getting {
